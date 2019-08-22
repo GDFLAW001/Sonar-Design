5b7321d28d01e8fde0f905eaddfc13f352d00ffe
Lawrence Godfrey <gdflaw001@myuct.ac.za>
2019-08-22 19:08:11 +0200

    changed sampling frequency. Works at 40kHz

--- Source Code/Chirp_Output/Chirp_Output.ino
+++ Source Code/Chirp_Output/Chirp_Output.ino
@@ -2,26 +2,24 @@
 
 #include "chirpout.h"
 
-#define DAC0(a) *(volatile int *)&(DAC0_DAT0L)=a
+#define DAC0(a) *(volatile int16_t *)&(DAC0_DAT0L)=a
 void setup() {
   analogWriteResolution(12);
 
-  extern int chirp [79910];
-  extern int sine [3700];
+  extern volatile uint16_t chirp[58374];
 
   SIM_SCGC2 |= SIM_SCGC2_DAC0; // enable DAC clock
   DAC0_C0 = DAC_C0_DACEN | DAC_C0_DACRFS; // enable the DAC module, 3.3V reference
 
    while (1){
-      for (int i = 0; i < 79910; i++){
+      for (int i = 0; i < 58374; i++){
         DAC0(chirp[i]);
       }
-      delay(10000);
+      delay(1000);
    }
 
     //noInterrupts();
 }
-
 
   
 void loop(){
